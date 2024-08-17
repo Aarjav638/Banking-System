e81d20cfa1176b20834273828f663e4f8a93ff20
Aarjav Jain <anshjain638@gmail.com>
2024-08-18 03:59:32 +0530

    adjusted dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,62 +1,41 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { Transaction } from "@/constants/types";
 import CustomCard from "@/components/Dashboard/CustomCard";
 import UserProfileCard from "@/components/Dashboard/UserProfileCard";
 import MiniStatement from "@/components/Dashboard/MiniStatement";
-import Fab from "@/components/ActionButton";
 export default function Dashboard() {
-  const [transactions, setTransactions] = useState<Transaction[]>([
-    {
-      id: "dhjcvabehjdfzbhjfbshjdvfbjkabdhjfbvhj",
-      amount: 10000,
-      type: "debit",
-      mode: "upi",
-    },
-    {
-      id: "vfdjhvbfjhdabvhjbadfjhzbvjhll",
-      amount: 50000,
-      type: "credit",
-      mode: "neft",
-    },
-  ]);
+  const [token, setToken] = useState<string | null>(null);
+  const [type, setType] = useState<string | null>(null);
   const router = useRouter();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
+      alert("You need to login first");
       router.push("/");
       return;
     }
-
-    const fetchTransactions = async () => {
-      const res = await fetch("/api/transactions", {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-
-      if (res.ok) {
-        const data = await res.json();
-        setTransactions(data);
-      }
-    };
-
-    fetchTransactions();
+    setToken(token);
   }, [router]);
 
   return (
     <div className="flex flex-col w-full h-full bg-slate-200 p-4 ">
-      <Fab />
       <div className="flex flex-col w-full justify-center items-center">
-        <h1 className="text-2xl sm:text-4xl text-center">Dashboard</h1>
+        <div className="flex items-center justify-center w-full">
+          <h1 className="my-4 text-2xl sm:text-4xl ">Dashboard</h1>
+        </div>
+        <p className="text-teal-300 text-md font-bold flex flex-col mb-4 bg-slate-500 w-4/5 md:w-3/4 lg:w-1/2 rounded-lg text-center p-4">
+          Welcome to your dashboard. Here you can see all your Details.
+        </p>
       </div>
-      <div className="mt-4 h-screen bg-black flex flex-col overflow-auto p-2">
-        <div className="justify-center h-3/4 md:h-1/2   md:justify-between flex flex-col md:flex-row p-4 ">
+      <div className="mt-10 h-screen bg-black flex flex-col overflow-auto p-2">
+        <div className="justify-center h-1/2 bg-yellow-50  md:justify-between flex flex-col md:flex-row p-4 ">
           <UserProfileCard type={type ? type : ""} token={token ? token : ""} />
           <CustomCard />
         </div>
-        <div className="w-full items-center flex flex-col gap-2 my-4 pb-4">
-          <form className="flex h-1/5 w-full justify-center items-center flex-col sm:flex-row gap-2 ">
+        <div className="h-1/2 w-full items-center flex flex-col gap-2 my-4 pb-4">
+          <form className="flex justify-center items-center flex-col sm:flex-row gap-2 ">
             <label
               htmlFor="accountType"
               className="mr-4 sm:text-center text-white"
@@ -73,7 +52,7 @@ export default function Dashboard() {
               <option value="current">Current</option>
             </select>
           </form>
-          <div className="mb-6 h-1/2 w-full flex flex-col">
+          <div className="mb-4">
             <MiniStatement token={token ? token : ""} type={type ? type : ""} />
           </div>
         </div>
